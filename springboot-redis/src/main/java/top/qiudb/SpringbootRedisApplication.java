e3f37304350743f583056f8c3c561b420407bee3
马英发 <1325554003@qq.com>
2021-09-08 20:03:14 +0800

    使用StringRedisTemplate整合Redis工具类

---
+++ springboot-redis/src/main/java/top/qiudb/SpringbootRedisApplication.java
@@ -0,0 +1,14 @@
+package top.qiudb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Dt
+ */
+@SpringBootApplication
+public class SpringbootRedisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRedisApplication.class, args);
+    }
+}
